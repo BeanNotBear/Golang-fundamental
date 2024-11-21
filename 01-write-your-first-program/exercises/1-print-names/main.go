cc920d11a31f9c1e251ff734a91f536c4022c2b0
NghiNV <nghinv03@gmail.com>
2024-11-21 21:00:05 +0700

    Question

--- 01-write-your-first-program/exercises/1-print-names/main.go
+++ 01-write-your-first-program/exercises/1-print-names/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// ---------------------------------------------------------
+// EXERCISE: Print names
+//
+//  Print your name and your best friend's name using
+//  Println twice
+//
+// EXPECTED OUTPUT
+//  YourName
+//  YourBestFriendName
+//
+// BONUS
+//  Use `go run` first.
+//  And after that use `go build` and run your program.
+// ---------------------------------------------------------
+
 func main() {
 	fmt.Println("Nghi")
 	fmt.Println("No One")
