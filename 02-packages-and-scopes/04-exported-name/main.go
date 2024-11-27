24a594b5460c279a789ef50f70194480a3605267
NghiNV <nghinv03@gmail.com>
2024-11-27 14:59:50 +0700

    exported name

---
+++ 02-packages-and-scopes/04-exported-name/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// in golang an exported name begin with a capital letter
+	// ex: math.Pi (P is capital letter)
+	// in this example Pi is an exported name
+	fmt.Println(math.Pi)
+}
